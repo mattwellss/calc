0cba9cf1d5069c1cf528db19e0ae6f3cba419116
Matthew Wells <matthewpaulwells@gmail.com>
2014-10-16 22:45:55 -0400

    Update view to show all operations added to calculation

--- src/Action/HomeAction.php
+++ src/Action/HomeAction.php
@@ -5,10 +5,13 @@ namespace App\Action;
 class HomeAction extends BaseAction
 {
     use \App\Traits\HasEngine;
+    use \App\Traits\HasCalculation;
 
     public function __invoke()
     {
         $this->response->content->set(
-            $this->engine->render('home'));
+            $this->engine->render('home', [
+                'operations' => $this->calculation->getOperations()
+            ]));
     }
 }
