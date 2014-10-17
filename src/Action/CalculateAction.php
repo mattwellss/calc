cb113387ed1f2f54341bde457a1b807613824809
Matthew Wells <matt@chalkfly.com>
2014-10-17 14:20:50 -0400

    Comment on actions

--- src/Action/CalculateAction.php
+++ src/Action/CalculateAction.php
@@ -7,6 +7,10 @@ class CalculateAction extends BaseAction
     use \App\Traits\HasFilter;
     use \App\Traits\HasCalculation;
 
+    /**
+     * Run the action
+     * @return void
+     */
     public function __invoke()
     {
         $this->response->headers->set('Content-type', 'application/json');
@@ -14,21 +18,25 @@ class CalculateAction extends BaseAction
         $post = (array)$this->request->post;
         $valid = $this->filter->values($post);
 
-        if ($valid) {
-            try {
-                $operation = $this->calculation->getOperation($post['operation']);
-                $result = $operation($post['number1'], $post['number2']);
-                $this->response->content->set(json_encode([
-                    'ok' => true,
-                    'result' => $result]));
-                return;
-            } catch (\Exception $e) {
-                //no operation?
-            }
+        if (!$valid) {
+            $this->response->headers->set('Status', 400);
+            $this->response->content->set(json_encode([
+                'ok' => false,
+                'result' => $this->filter->getMessages()]));
+            return;
         }
-        $this->response->headers->set('Status', 400);
-        $this->response->content->set(json_encode([
-            'ok' => false,
-            'result' => $this->filter->getMessages()]));
+
+        try {
+            $operation = $this->calculation->getOperation($post['operation']);
+            $result = $operation($post['number1'], $post['number2']);
+            $this->response->content->set(json_encode([
+                'ok' => true,
+                'result' => $result]));
+        } catch (\Exception $e) {
+            $this->response->headers->set('Status', 500);
+            $this->response->content->set(json_encode([
+                'ok' => false,
+                'result' => $e->getMessage()]));
+        }
     }
 }
